518364395eaff4231fd618f39105c4e0b6a1a20d
maxwen <max.weninger@gmail.com>
2020-05-24 13:40:48 +0200

    SystemUI: navbar extra keys
    power, volup, voldown
    
    nift4: port to 13
    
    Change-Id: I7cfcd30b5d49001e97aa85c284220f0fa7e556dc
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/navigationbar/views/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/views/NavigationBarView.java
@@ -138,6 +138,9 @@ public class NavigationBarView extends FrameLayout
     private Context mLightContext;
     private int mLightIconColor;
     private int mDarkIconColor;
+    private KeyButtonDrawable mPowerButton;
+    private KeyButtonDrawable mVolumePlusButton;
+    private KeyButtonDrawable mVolumeMinusButton;
 
     private EdgeBackGestureHandler mEdgeBackGestureHandler;
     private DisplayTracker mDisplayTracker;
@@ -354,6 +357,9 @@ public class NavigationBarView extends FrameLayout
         mButtonDispatchers.put(R.id.menu_container, mContextualButtonGroup);
         mButtonDispatchers.put(R.id.dpad_left, cursorLeftButton);
         mButtonDispatchers.put(R.id.dpad_right, cursorRightButton);
+        mButtonDispatchers.put(R.id.power, new ButtonDispatcher(R.id.power));
+        mButtonDispatchers.put(R.id.volume_minus, new ButtonDispatcher(R.id.volume_minus));
+        mButtonDispatchers.put(R.id.volume_plus, new ButtonDispatcher(R.id.volume_plus));
         mDeadZone = new DeadZone(this);
 
         mShowCursorKeysObserver = new ContentObserver(null) {
@@ -486,6 +492,18 @@ public class NavigationBarView extends FrameLayout
         return mButtonDispatchers.get(R.id.dpad_right);
     }
 
+    public ButtonDispatcher getPowerButton() {
+        return mButtonDispatchers.get(R.id.power);
+    }
+
+    public ButtonDispatcher getVolumePlusButton() {
+        return mButtonDispatchers.get(R.id.volume_plus);
+    }
+
+    public ButtonDispatcher getVolumeMinusButton() {
+        return mButtonDispatchers.get(R.id.volume_minus);
+    }
+
     public SparseArray<ButtonDispatcher> getButtonDispatchers() {
         return mButtonDispatchers;
     }
@@ -524,6 +542,9 @@ public class NavigationBarView extends FrameLayout
         if (orientationChange || densityChange || dirChange) {
             mBackIcon = getBackDrawable();
         }
+        mPowerButton = getDrawable(R.drawable.ic_sysbar_power);
+        mVolumePlusButton = getDrawable(R.drawable.ic_sysbar_volume_plus);
+        mVolumeMinusButton = getDrawable(R.drawable.ic_sysbar_volume_minus);
     }
 
     /**
@@ -667,6 +688,17 @@ public class NavigationBarView extends FrameLayout
                 || isImeRenderingNavButtons() || !disableCursorKeys;
         mContextualButtonGroup.setButtonVisibility(R.id.ime_switcher, !disableImeSwitcher);
 
+
+        if (getPowerButton() != null) {
+            getPowerButton().setImageDrawable(mPowerButton);
+        }
+        if (getVolumeMinusButton() != null) {
+            getVolumeMinusButton().setImageDrawable(mVolumeMinusButton);
+        }
+        if (getVolumePlusButton() != null) {
+            getVolumePlusButton().setImageDrawable(mVolumePlusButton);
+        }
+
         mBarTransitions.reapplyDarkIntensity();
 
         boolean disableHome = isGesturalMode(mNavBarMode)
@@ -712,6 +744,17 @@ public class NavigationBarView extends FrameLayout
         getHomeHandle().setVisibility(disableHomeHandle || mHomeHandleForceHidden ? View.INVISIBLE : View.VISIBLE);
         getCursorLeftButton().setVisibility(disableCursorKeys  ? View.INVISIBLE : View.VISIBLE);
         getCursorRightButton().setVisibility(disableCursorKeys ? View.INVISIBLE : View.VISIBLE);
+
+        if (getPowerButton() != null) {
+            getPowerButton().setVisibility(View.VISIBLE);
+        }
+        if (getVolumeMinusButton() != null) {
+            getVolumeMinusButton().setVisibility(View.VISIBLE);
+        }
+        if (getVolumePlusButton() != null) {
+            getVolumePlusButton().setVisibility(View.VISIBLE);
+        }
+
         notifyActiveTouchRegions();
     }
 
